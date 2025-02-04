2e60ca6fe3fccd20fe63eb084539b49a47ee01e2
ranit958 <ranitmaity958@gmail.com>
2025-02-04 15:05:59 +0530

    added required changes

---
+++ src/components/model_trainer.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression,Ridge,Lasso,ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+from src.utils import evaluate_mode
+
+from dataclasses import dataclass
+import sys
+import os
+
+
+@dataclass
+class ModelTraineConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTraineConfig()
+
+    def initiate_model_training(self,train_array,test_array):
+        try:
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train,y_train,X_test,y_test =(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1],
+            )
+
+            models = {
+                'LinearRegression': LinearRegression(),
+                'Ridge': Ridge(),
+                'Lasso': Lasso(),
+                'ElasticNet': ElasticNet(),
+                'DecisionTree' : DecisionTreeRegressor()
+            }
+
+            model_report:dict = evaluate_mode(X_train,y_train,X_test,y_test,models)
+            print(model_report)
+            print('\n=====================================================================\n')
+            logging.info(f'Model Report : {model_report}')
+
+            # To get best model score from dictionary
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+
+            print(f'Best Model Found, Best Model Name : {best_model_name}, R2 Score : {best_model_score}')
+            print('\n=====================================================================\n')
+            logging.info(f'Best Model Found, Best Model Name : {best_model_name}, R2 Score : {best_model_score}')
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+
+        except Exception as e:
+            logging.info('Exception occured at Model Training')
+            raise CustomException(e,sys)
+
+
+
+
+
---
+++ src/utils.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_mode(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            # Train Model
+            model.fit(X_train,y_train)
+
+            # Predict test data
+            y_test_pred = model.predict(X_test)
+
+            # Get R2_score
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+    
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        logging.info('Exception occured while loading object in load_object method of util')
+        raise CustomException(e,sys)
